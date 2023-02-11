e9461272e25f3efe53b83722b43e0dc58a2bb469
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-02-11 13:39:02 +0300

    implement registration and login logic

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,18 +1,53 @@
 import { Request, Response, NextFunction } from "express";
 
 const ApiError = require("../error/ApiError.ts");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req: Request, res: Response) {}
-
-  async login(req: Request, res: Response) {}
-
-  async check(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("ID is not defined"));
+  async registration(req: Request, res: Response, next: NextFunction) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Некорректный email или пароль"));
     }
-    res.json(id);
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользователь с таким email уже существует")
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal("Пользователь не найден"));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Указан неверный пароль"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+
+  async check(req, res: Response, next: NextFunction) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
