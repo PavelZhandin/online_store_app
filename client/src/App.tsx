d28fa2bb42ea9474fef652cbce161074b2031bf8
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-02-24 11:13:58 +0300

    configure tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,5 @@
-import React from "react";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
