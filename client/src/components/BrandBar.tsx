0f22d0314de75dd77ac404f9b0bc8218b11e5f40
Pavel_Zhandin <pavelzhandin@gmail.com>
2023-02-25 19:01:10 +0300

    add BrandBar

---
+++ client/src/components/BrandBar.tsx
@@ -0,0 +1,31 @@
+import { Card, ListGroup } from "flowbite-react";
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Context } from "..";
+
+const BrandBar = observer(() => {
+  const { device } = useContext(Context);
+  return (
+    <>
+      <ListGroup className="flex flex-wrap">
+        {device?.brands.map((brand: any) => (
+          <Card
+            style={{
+              borderColor: brand.id === device.selectedBrand ? "lightBlue" : "",
+            }}
+            onClick={() => (device.selectedBrand = brand.id)}
+            key={brand.id}
+            href="#"
+            className="m-3"
+          >
+            <h5 className="font-bold tracking-tight text-gray-900 dark:text-white">
+              {brand.name}
+            </h5>
+          </Card>
+        ))}
+      </ListGroup>
+    </>
+  );
+});
+
+export default BrandBar;
